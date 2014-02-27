c74b73cb09f4a9a570c0e729041687c501152944
agaye <ag13748@it017324.(none)>
2014-02-27 14:46:52 +0000

    Removed 'omxSymbolTable' and created 'mxAlgebra'.

---
+++ R/mxAlgebra.R
@@ -0,0 +1,19 @@
+#' 
+#' @title loads 'omxSymbolTable' and runs 'mxAlgebra'
+#' @param expression An R expression of OpenMx-supported matrix operators and
+#' matrix functions.
+#' @param name An optional character string indicating the name of the
+#' object.
+#' @param dimnames list, the dimnames attribute for the algebra: a list of
+#' length 2 giving the row and column names respectively. An
+#' empty list is treated as NULL, and a list of length one as
+#' row names. The list can be named, and the list names will be
+#' used as names for the dimensions.
+#' @description this is exactly the same as the function in 'OpenMx' except for the fact that
+#' it load 'omxSymbolTable'.
+#' @export
+#' 
+mxAlgebra <- function (expression, name = NA, dimnames = NA){
+  data(omxSymbolTable)
+  OpenMx::mxAlgebra(expression, name, dimnames)
+}
