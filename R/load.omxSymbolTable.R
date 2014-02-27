271ea64c8dd23970c991df9cc3609a74d76234e2
agaye <ag13748@it017324.(none)>
2014-02-27 14:19:48 +0000

    Some change to make it work.

--- R/load.omxSymbolTable.R
+++ R/load.omxSymbolTable.R
@@ -1,9 +1,11 @@
 #' 
 #' @title loads the 'omxSymbolTable'
+#' @param a just a dummy
 #' @description not important
 #' @export
 #' 
-load.omxSymbolTable <- function (...){
+load.omxSymbolTable <- function (a="l"){
+  dummy <- a
   library('OpenMx')
   data(omxSymbolTable)
 }
