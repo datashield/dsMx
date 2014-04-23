5067219f2f1ccc7f5ec2fa4b7723e1253b9e8ab3
agaye <ag13748@it017324.(none)>
2014-04-23 16:12:14 +0100

    Description file altered

--- R/mxAlgebraDS.R
+++ R/mxAlgebraDS.R
@@ -1,6 +1,6 @@
 #' 
 #' @title loads 'omxSymbolTable' and runs 'mxAlgebra'
-#' @param objs, a vector of the two objects add up
+#' @param expr, an R expression of OpenMx-supported matrix operators and matrix functions 
 #' @param name An optional character string indicating the name of the
 #' object.
 #' @param dimnames list, the dimnames attribute for the algebra: a list of
@@ -12,10 +12,10 @@
 #' it load 'omxSymbolTable'.
 #' @export
 #' 
-mxAlgebraDS <- function (objs, name = NA, dimnames = NA){
+mxAlgebraDS <- function (expr, name = NA, dimnames = NA){
   data(omxSymbolTable)
   n <- name
   d <- dimnames
-  output <- OpenMx::mxAlgebra(quote(objs[1]) + quote(objs[2]), name=n, dimnames=d)
+  output <- OpenMx::mxAlgebra(expr, name=n, dimnames=d)
   return(output)
 }
