19384e040b66f09a73c54ff4985e813971cf1dbf
agaye <ag13748@it017324.(none)>
2014-04-25 12:16:21 +0100

    Back to basics.

--- R/loadomxSymbolTableDS.R
+++
@@ -1,9 +0,0 @@
-#' 
-#' @title loads 'omxSymbolTable' from the paqckage 'OpenMx'
-#' @description just to load this flipping table
-#' @details said above
-#' @export
-#' 
-loadomxSymbolTableDS <- function (){
-  data(omxSymbolTable)
-}
