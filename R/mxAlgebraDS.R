07aeb5f83860be5e8e06860056b68cb6078721e9
agaye <ag13748@it017324.(none)>
2014-04-25 12:56:03 +0100

    New version of 'mxAlgebraDS' added.

---
+++ R/mxAlgebraDS.R
@@ -0,0 +1,20 @@
+#'
+#' @title loads 'omxSymbolTable' and runs 'mxAlgebra'
+#' @param expr a character, the expression to evaluate
+#' @param name an optional character string indicating the name of the
+#' object.
+#' @param dimnames list, the dimnames attribute for the algebra: a list of
+#' length 2 giving the row and column names respectively. An
+#' empty list is treated as NULL, and a list of length one as
+#' row names. The list can be named, and the list names will be
+#' used as names for the dimensions.
+#' @description this is exactly the same as the function in 'OpenMx' except that it 
+#' provides the workaround to the difficulty of loading/and/or using omxSymbolTable
+#' from the client side
+#' @export
+#'
+mxAlgebraDS <- function (expr, name = NA, dimnames = NA){
+  myexpr <- paste0("mxAlgebra(", expr,", '", name, "', ", dimnames, ")")
+  output <- eval(parse(text=myexpr))
+  return(output)
+}
