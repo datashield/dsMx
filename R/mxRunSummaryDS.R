1a34f466b62ebd72f2833a84199925cbd1de92fc
agaye <ag13748@it017324.(none)>
2014-09-23 12:35:26 +0100

    Some code commented for debugging.

--- R/mxRunSummaryDS.R
+++ R/mxRunSummaryDS.R
@@ -18,18 +18,15 @@ mxRunSummaryDS <- function (input){
   # if(length(idx1) > 0){ output <- output[-idx1] }
   
   # remove the min and max values from the summary
-  idx2 <- which(names(output) == "dataSummary")
-  if(length(idx2) > 0){
-    l <- length(output[idx2])
-    for(i in 1:l){
-      lrw <- dim(output$dataSummary[[i]])[1]
-      output$dataSummary[[i]][1,] <- c(rep("Min.   :------  ",2))
-      output$dataSummary[[i]][lrw,] <- c(rep("Max.   :------  ",2))
-    }
-  }
-  outputf <- vector('list', length(output))
-  for(i in 1:length(output)){
-    outputf[[i]] <- output[i]
-  }
-  return(outputf)
+  #idx2 <- which(names(output) == "dataSummary")
+  #if(length(idx2) > 0){
+    #l <- length(output[idx2])
+    #for(i in 1:l){
+      #lrw <- dim(output$dataSummary[[i]])[1]
+      #output$dataSummary[[i]][1,] <- c(rep("Min.   :------  ",2))
+      #output$dataSummary[[i]][lrw,] <- c(rep("Max.   :------  ",2))
+    #}
+  #}
+
+  return(output)
 }
