82e887e81f8a4650540c68dd8415ad58a780a44b
agaye <ag13748@it017324.(none)>
2014-09-23 11:50:55 +0100

    Bud sorted.

--- R/mxRunSummaryDS.R
+++ R/mxRunSummaryDS.R
@@ -13,11 +13,11 @@ mxRunSummaryDS <- function (input){
   output <- summary(input)
   
   # remove non relevant information
-  idx1 <- which(names(aa) %in% c("frontendTime", "backendTime", "independentTime", "wallTime", "cpuTime"))
-  if(length(idx1) > 0){ output <- output[-idx1] }
+  # idx1 <- which(names(output) %in% c("frontendTime", "backendTime", "independentTime", "wallTime", "cpuTime"))
+  # if(length(idx1) > 0){ output <- output[-idx1] }
   
   # remove the min and max values from the summary
-  idx2 <- which(names(aa) == "dataSummary")
+  idx2 <- which(names(output) == "dataSummary")
   if(length(idx2) > 0){
     l <- length(output[idx2])
     for(i in 1:l){
