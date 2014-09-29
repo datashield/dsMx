5cdfac2f7df2912e04e2ced4d97b4a0f2e0ba9e2
agaye <ag13748@it017324.(none)>
2014-09-29 17:03:52 +0100

    'mxAlgebraDS' amended.

--- R/mxAlgebraDS.R
+++ R/mxAlgebraDS.R
@@ -1,6 +1,7 @@
 #'
 #' @title Runs 'mxAlgebra'
-#' @param expr a character, the expression to evaluate
+#' @param expr a list which contains the objects to carry out a caluclation for
+#' and the algebraic operator or null if no operator symbol is required.
 #' @param name an optional character string indicating the name of the
 #' object.
 #' @param dimnames list, the dimnames attribute for the algebra: a list of
@@ -15,7 +16,27 @@
 #' @author A. Gaye
 #' 
 mxAlgebraDS <- function (expr, name = NA, dimnames = NA){
-  myexpr <- paste0("mxAlgebra(", expr,", '", name, "', ", dimnames, ")")
+  symbol <- expr[[2]]
+  operators <- c("hat","percAsterix","asterix","percX","percAdd","slash","plus","percHat")
+  if(expr[[2]] %in% operators){
+    if(symbol=="hat"){ symbol <- "^" }
+    if(symbol=="percAsterix"){ symbol <- "%*%" }
+    if(symbol=="asterix"){ symbol <- "*" }
+    if(symbol=="percX"){ symbol <- "%x%" }
+    if(symbol=="percAdd"){ symbol <- "%&%" }
+    if(symbol=="slash"){ symbol <- "/" }
+    if(symbol=="plus"){ symbol <- "+" }
+    if(symbol=="percHat"){ symbol <- "%^%" }
+    str <- expr[[1]][1]
+    for(i in 2:length(expr[[1]])){
+      str <- paste0(str,symbol,str <- expr[[1]][i])
+    }
+    myexpr <- paste0("mxAlgebra(", str,", '", name, "', ", dimnames, ")")
+  }else{
+    str <- expr[[1]]
+    myexpr <- paste0("mxAlgebra(", symbol, "(", paste(str, sep=","), ")",  ", '", name, "', ", dimnames, ")")
+  }
+  
   library(OpenMx)
   output <- eval(parse(text=myexpr))
   return(output)
