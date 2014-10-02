f53846aeb105f80543730b4fd8d355698a7bc01e
agaye <ag13748@it017324.(none)>
2014-10-02 17:43:40 +0100

    Typo corrected.

--- R/mxDataDS.R
+++ R/mxDataDS.R
@@ -24,7 +24,7 @@
 #' 
 mxDataDS <- function(observed, type, means, numObs){
   # this filter sets the minimum number of observations that are allowed 
-  nfilter <- setFilterDS()
+  nfilter <- setFilter()
   
   # call the OpenMx 'mxData' function
   library(OpenMx)
