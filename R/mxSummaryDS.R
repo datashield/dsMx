ce687c3947d597dd13f936cc99dae462d266908d
agaye <ag13748@it017324.(none)>
2014-10-09 17:02:59 +0100

    Function 'mxSummaryDS' completed.

--- R/mxSummaryDS.R
+++ R/mxSummaryDS.R
@@ -15,19 +15,19 @@ mxSummaryDS <- function (input){
   library(OpenMx)
   output <- OpenMx::summary(input)
   # remove non relevant information
-  #idx1 <- which(names(output) %in% c("frontendTime", "backendTime", "independentTime", "wallTime", "cpuTime"))
-  #if(length(idx1) > 0){ output <- output[-idx1] }
+  idx1 <- which(names(output) %in% c("frontendTime", "backendTime", "independentTime", "wallTime", "cpuTime"))
+  if(length(idx1) > 0){ output <- output[-idx1] }
   
   # remove the min and max values from the summary
-  #idx2 <- which(names(output) == "dataSummary")
-  #if(length(idx2) > 0){
-  #  l <- length(output[idx2])
-  #  for(i in 1:l){
-  #    lrw <- dim(output$dataSummary[[i]])[1]
-  #    output$dataSummary[[i]][1,] <- c(rep("Min.   :------  ",2))
-  #    output$dataSummary[[i]][lrw,] <- c(rep("Max.   :------  ",2))
-  #  }
-  #}
-  print(output)
+  idx2 <- which(names(output) == "dataSummary")
+  if(length(idx2) > 0){
+    l <- length(output[idx2][[1]])
+    for(i in 1:l){
+      lrw <- dim(output$dataSummary[[i]])[1]
+      output$dataSummary[[i]][1,] <- c(rep("Min.   :------  ",2))
+      output$dataSummary[[i]][lrw,] <- c(rep("Max.   :------  ",2))
+    }
+  }
+
   return(output)
 }
